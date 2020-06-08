941495c01d7f46028e83d75ad34ba17b2626f9cf
mramsterdam85 <hunggs.no7@gmail.com>
2020-06-08 23:50:35 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function greeting(msg) {
+  return function who(name) {
+    console.log(`${msg} - ${name}`);
+  }
+}
 
-function
+var hello = greeting('hello');
+
+hello('kevin');
+hello('Grace');
+
+function counter(step=1) {
+  var count = 1;
+  return function cal() {
+    count += step;
+    return count;
+  }
+}
+
+var inc1 = counter(1);
+console.log(inc1());
+console.log(inc1());
+
+var inc12 = counter(1);
+console.log(inc12());
+
+var b1 = document.getElementById('b1');
+var b2 = document.getElementById('b2');
+var b3 = document.getElementById('b3');
+
+var buttons = [b1, b2, b3];
+
+for(let i = 0; i<buttons.length ; i ++) {
+  buttons[i].addEventListener('click', function onClick() {
+    console.log(`click on button ${i}`);
+  });
+  console.log(i);
+}
